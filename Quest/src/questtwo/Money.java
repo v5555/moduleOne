c8de94a73d2f4313dff4657088ccb9730752ccd5
v5555 <@MICROSOF-C5CEA3>
2016-11-08 14:29:17 +0300

    1

--- Quest/src/questtwo/Money.java
+++ Quest/src/questtwo/Money.java
@@ -1,10 +1,25 @@
 package questtwo;
-
+import java.util.Scanner;
 public class Money {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Vvedite summu");
+		int money = scan.nextInt();
+		int mA = money % 10;
+		int mB = money % 100;
+		scan.close();
+		if ((mA==0) || (mA>=5) && (mA<=9) || (mB>=11) && (mB<=14)) {
+			System.out.println("Y vas " + money + " rubley");
+		}
+			else if ((mA<=0) && (mA>=3) || (mB>=5) && (mB<=11) || (mA==1) ){
+				System.out.println("Y vas " + money + " rubl");
+			}
+				if ((mA>=2) && (mA<=4)) {
+					System.out.println("Y vas " + money + " rublya");					
+				}
+			}
+		}
 
-	}
 
-}
+
