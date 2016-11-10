576aae65a0e01de67ace78fd1fb11d54ff7e4033
v5555 <@MICROSOF-C5CEA3>
2016-11-10 12:24:04 +0300

    ready

--- Quest/src/questtwo/dayOfWeek.java
+++ Quest/src/questtwo/dayOfWeek.java
@@ -1,10 +1,24 @@
 package questtwo;
-
+import java.util.Scanner;
 public class dayOfWeek {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a number of day");
+		int day = scan.nextInt();
+		String[] dayOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+		scan.close();
+		if (day<0 && day>8); {
+			System.out.println(dayOfWeek[day-1]);
+		}  if(day<8); {  System.out.println("Wrong number");
+		}
+		
 	}
-
 }
+	
+		
+
+
+
+
+
